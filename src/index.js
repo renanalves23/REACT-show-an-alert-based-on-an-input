367b33ff1bff7990426473934b824ab2f3f9c6e9
RENAN ALVES CARVALHO MENDES <renanalves23@outlook.com>
2020-02-29 11:29:07 -0300

    event handler and alert OK
    working

--- src/index.js
+++ src/index.js
@@ -1,14 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import ScotchInfoBar from './ScotchInfoBar';
-import './styles.css';
+import React, { useState } from "react";
+import ReactDOM from "react-dom";
+import ScotchInfoBar from "./ScotchInfoBar";
+import "./styles.css";
 
 function App() {
+  const [phrase, setPhrase] = useState("");
+
+  if (phrase === "open sessame") alert("You may pass");
+
   return (
     <div className="App">
       <h2>What's the secret phrase?</h2>
 
-      <input type="text" placeholder="Super duper secret" />
+      <input
+        type="text"
+        placeholder="Super duper secret"
+        value={phrase}
+        onChange={event => setPhrase(event.target.value)}
+      />
 
       <p>
         Hint: It's <strong>open sesame</strong>
@@ -19,5 +28,5 @@ function App() {
   );
 }
 
-const rootElement = document.getElementById('root');
+const rootElement = document.getElementById("root");
 ReactDOM.render(<App />, rootElement);
